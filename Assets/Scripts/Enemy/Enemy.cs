54241a2ce7c6e4d5d35eaf13ea73f86532fd8469
lne Euler <3149512122@qq.com>
2023-09-07 17:27:37 +0800

    墙体判断

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -6,16 +6,17 @@ public class Enemy : MonoBehaviour
 {
     [Header("基本组件")]
     Rigidbody2D rb;
-    Animator anim;
+    protected Animator anim;
     [Header("基本参数")]
     public float normalSpeed;
     public float chaseSpeed;
     public float currentSpeed;
-
+    public Vector3 faceDir;
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        currentSpeed = normalSpeed;
     }
 
     // Start is called before the first frame update
@@ -27,11 +28,20 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // -1 面向左
+        faceDir = new Vector3(-1 * transform.localScale.x, 0, 0);
+
+        if(PhysicsCheck.)
     }
 
-    public void Move()
+    void FixedUpdate()
     {
-
+        Move();
     }
+
+    public virtual void Move()
+    {
+        rb.velocity = new Vector2(currentSpeed * faceDir.x * Time.deltaTime, rb.velocity.y);
+    }
+
 }
--- Assets/Scripts/General/PhysicsCheck.cs
+++ Assets/Scripts/General/PhysicsCheck.cs
@@ -1,15 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class PhysicsCheck : MonoBehaviour
 {
+    private CapsuleCollider2D coll;
     [Header("状态")]
     public bool isGround;
     [Header("检测参数")]
+    public bool manual;
     public float checkRadius;
     public LayerMask groundLayer;
     public Vector2 bottomOffset;    //位移差值检测
+
+    public bool touchLeftWall;
+    public Vector2 leftOffset;
+    public bool touchRightWall;
+    public Vector2 rightOffset;
+
+    private void Awake() {
+        coll = GetComponent<CapsuleCollider2D>();
+        if(!manual)
+        {
+            // bounds代表的是世界坐标
+            rightOffset = new Vector2((coll.bounds.size.x + coll.offset.x) / 2, coll.bounds.size.y / 2);
+            leftOffset = new Vector2(-(coll.bounds.size.x + coll.offset.x) / 2, coll.bounds.size.y / 2);
+        }
+    }
     private void Update()
     {
         Check();
@@ -19,6 +37,10 @@ public class PhysicsCheck : MonoBehaviour
         // 检测地面
         isGround = Physics2D.OverlapCircle((Vector2)transform.position + bottomOffset, checkRadius, groundLayer);
         // 强制类型转换
+
+        touchLeftWall = Physics2D.OverlapCircle((Vector2)transform.position + leftOffset, checkRadius, groundLayer);
+        touchRightWall = Physics2D.OverlapCircle((Vector2)transform.position + rightOffset, checkRadius, groundLayer);
+
     }
 
     // private void OnDrawGizmosSelected()
@@ -26,5 +48,7 @@ public class PhysicsCheck : MonoBehaviour
     //     //选中的时候绘制
     //     //绘制一个空心球
     //     Gizmos.DrawWireSphere((Vector2)transform.position + bottomOffset, checkRadius);
+    //     Gizmos.DrawWireSphere((Vector2)transform.position + leftOffset, checkRadius);
+    //     Gizmos.DrawWireSphere((Vector2)transform.position + rightOffset, checkRadius);
     // }
 }
