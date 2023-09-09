2d6604af00f9e9d80a9cd8465591a36be180c576
Euler_lne <3149512122@qq.com>
2023-09-09 14:42:14 +0800

    1

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -7,22 +7,30 @@ public class Enemy : MonoBehaviour
     [Header("基本组件")]
     Rigidbody2D rb;
     protected Animator anim;
+    PhysicsCheck physicsCheck;
     [Header("基本参数")]
     public float normalSpeed;
     public float chaseSpeed;
     public float currentSpeed;
     public Vector3 faceDir;
+    public Transform attacker;
+
+    [Header("计时器")]
+    public float waitTime;
+    public float waitTimeCounter;
+    public bool wait;
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        physicsCheck = GetComponent<PhysicsCheck>();
         currentSpeed = normalSpeed;
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        waitTimeCounter = waitTime;
     }
 
     // Update is called once per frame
@@ -31,7 +39,28 @@ public class Enemy : MonoBehaviour
         // -1 面向左
         faceDir = new Vector3(-1 * transform.localScale.x, 0, 0);
 
-        if(PhysicsCheck.)
+        if((physicsCheck.touchLeftWall && faceDir.x < 0) || (physicsCheck.touchRightWall && faceDir.x > 0))
+        {
+            wait = true;
+            anim.SetBool("walk", false);
+        }
+
+        TimeCounter();
+
+    }
+
+    public void TimeCounter()
+    {
+        if(wait)
+        {
+            waitTimeCounter -= Time.deltaTime;
+            if(waitTimeCounter <= 0)
+            {
+                wait = false;
+                waitTimeCounter = waitTime;
+                transform.localScale = new Vector3(faceDir.x, 1, 1);
+            }
+        }
     }
 
     void FixedUpdate()
@@ -44,4 +73,12 @@ public class Enemy : MonoBehaviour
         rb.velocity = new Vector2(currentSpeed * faceDir.x * Time.deltaTime, rb.velocity.y);
     }
 
+    public void OnTakeDamage(Transform attackTrans)
+    {
+        attacker = attackTrans;
+        if (attackTrans.position.x - transform.position.x > 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+        if(attackTrans.position.x - transform.position.x < 0)
+            transform.localScale = new Vector3(1, 1, 1); 
+    }
 }
